22f0fb2e5fff0d9ae160c9052b235babfc76e6a6
ahmedyousry0098 <ahmedyousry098@gmail.com>
2023-08-04 23:10:22 +0300

    update-product-page

--- product/products.script.js
+++ product/products.script.js
@@ -1,144 +1,119 @@
 let products;
-const mainContainer = document.getElementById('mainContainer')
-
-async function getProduct () {
-    products = await fetch('https://fakestoreapi.com/products')
-    .then(data => {
-        return data.json()   
+const productImageContainer = document.getElementById("image-container");
+async function getProduct() {
+  products = await fetch("https://fakestoreapi.com/products")
+    .then((data) => {
+      return data.json();
     })
-    .catch(err => {
-        console.log({error: err})
-    })
+    .catch((err) => {
+      console.log({ error: err });
+    });
 
-    const queryParam = window.location.search
-    const urlParams = new URLSearchParams(queryParam);
-    let productId = urlParams.get('id') || 1
-    if (productId <= 0) productId = 1
+  const queryParam = window.location.search;
+  const urlParams = new URLSearchParams(queryParam);
+  let productId = urlParams.get("id") || 1;
+  if (productId <= 0) productId = 1;
 
-    const clickedProduct = products.find(pro => pro.id == productId) 
-    return clickedProduct
+  const clickedProduct = products.find((pro) => pro.id == productId);
+  return clickedProduct;
 }
-
 function initProductImg(product) {
-    const prodImg = document.createElement('img')
-    prodImg.src = product.image
-    prodImg.classList.add('prodImg')
-    mainContainer.appendChild(prodImg)
+  const prodImgDivContainer = document.createElement("div");
+  prodImgDivContainer.className =
+    "border rounded-4 mb-3 d-flex justify-content-center";
+  const prodImg = document.createElement("img");
+  prodImg.className = "rounded-4 fit";
+  prodImg.style = "max-width: 100%; max-height: 80vh; margin: auto";
+  prodImg.src = product.image;
+  const imageAnchr = document.createElement("a");
+  imageAnchr.href = product.image;
+  imageAnchr.class = "rounded-4";
+  imageAnchr.target = "_blank";
+  prodImgDivContainer.appendChild(imageAnchr);
+  imageAnchr.appendChild(prodImg);
+  productImageContainer.appendChild(prodImgDivContainer);
 }
-
+function initSmallProductImgs(product) {
+  const smallProductsImgsDivContainer = document.createElement("div");
+  smallProductsImgsDivContainer.className =
+    "d-flex justify-content-center mb-3";
+  for (let index = 0; index < 5; index++) {
+    const smallImageAnchr = document.createElement("a");
+    smallImageAnchr.className = "border mx-1 rounded-2 item-thumb";
+    smallImageAnchr.target = "_blank";
+    smallImageAnchr.href = product.image;
+    const smallProdImg = document.createElement("img");
+    smallProdImg.className = "rounded-2";
+    smallProdImg.style = "width 60px; height: 60px";
+    smallProdImg.src = product.image;
+    smallImageAnchr.append(smallProdImg);
+    smallProductsImgsDivContainer.appendChild(smallImageAnchr);
+    productImageContainer.appendChild(smallProductsImgsDivContainer);
+  }
+}
 function initProductInfo(product) {
-    // div
-    const prodInfoDiv = document.createElement('div')
-    prodInfoDiv.classList.add('infoContainer')
-
-    // title 
-    const titlePar = document.createElement('p')
-    titlePar.classList.add('txt', 'bigTxt')
-    titlePar.innerHTML = `${product.title}`
-
-    // description
-    const descPar = document.createElement('p')
-    descPar.classList.add('txt')
-    descPar.innerHTML = `Description: ${product.description}`
-
-    // category
-    const categorPar = document.createElement('p')
-    categorPar.classList.add('txt')
-    categorPar.innerHTML = `Category: ${product.category}`
-    
-    // category
-    const pricePar = document.createElement('p')
-    pricePar.classList.add('txt')
-    pricePar.innerHTML = `Price: ${product.price}$`
-
-    // Rating
-    const ratingDivContainer = document.createElement('div')
-
-    const ratingPar = document.createElement('p')
-    ratingPar.innerHTML = `${product.rating.rate}`
-    ratingPar.style.fontWeight = '700'
-    ratingPar.style.marginLeft = "5px"
-    
-    const star = document.createElement('span')
-    star.innerHTML = `&bigstar;`
-    star.style.color = 'rgb(255,216,20)'
-    star.style.fontSize = "20px"
-    
-    ratingPar.prepend(star)
-
-    const absoluteRatingBar = document.createElement('div')
-    const actualRatingBar = document.createElement('div')
-
-    absoluteRatingBar.classList.add('absoluteRatingBar')
-    actualRatingBar.classList.add('actualRatingBar')
-    actualRatingBar.style.width = `${(product.rating.rate*20)}%`
-
-    absoluteRatingBar.appendChild(actualRatingBar)
-
-    ratingDivContainer.classList.add('ratingContainer')
-    ratingDivContainer.appendChild(ratingPar)
-    ratingDivContainer.appendChild(absoluteRatingBar)
-
-
-    // append Elements Into Info div
-    prodInfoDiv.appendChild(titlePar)
-    prodInfoDiv.appendChild(descPar)
-    prodInfoDiv.appendChild(categorPar)
-    prodInfoDiv.appendChild(pricePar)
-    prodInfoDiv.appendChild(ratingDivContainer)
-    mainContainer.appendChild(prodInfoDiv)
+  const productTitle = document.getElementById("product-title");
+  productTitle.innerHTML = product.title;
 }
-
-function initAddToCartBtn (product) {
-    const btn = document.createElement('button')
-    btn.classList.add('addCartBtn')
-    const btnTitle = document.createTextNode('Add To Cart')
-
-    const pricePar = document.createElement('p')
-    const priceTxt = document.createTextNode(`${product.price}$`)
-    pricePar.classList.add('priceOnBtn')
-    pricePar.appendChild(priceTxt)
-
-    btn.appendChild(btnTitle)
-    btn.appendChild(pricePar)
-
-    btn.addEventListener('click', () => {
-        const cratIds = localStorage.getItem("cartIds")
-        if (!cratIds) {
-            localStorage.setItem('cartIds', JSON.stringify(product.id))
-        } else {
-            localStorage.setItem('cartIds', `${cratIds},${product.id}`)            
-        }        
-        initPopupAlert(`Item ${product.title} added successfully to cart`)
-        setTimeout(() => {
-            document.getElementById('alertBox').remove()
-        }, 2000)
-    })
-
-    mainContainer.appendChild(btn)
+function initProductRating(product) {
+  const productRatingData = document.getElementById("rating-div");
+  const rate = document.getElementById("rate");
+  const ratedBy = document.getElementById("ratedBy");
+  const price = document.getElementById("price");
+  const productDescription = document.getElementById("product-Description");
+  const category = document.getElementById("category");
+  let rating = product.rating.rate;
+  console.log(rating);
+  let stars = document.createElement("span");
+  for (i = 0; i < 5; i++) {
+    if (i < rating) {
+      stars.innerHTML += `<i class="fa fa-star text-warning"></i>`;
+    } else {
+      stars.innerHTML += `<i class="fa fa-star"></i>`;
+    }
+  }
+  productRatingData.append(stars);
+  rate.innerHTML = product.rating.rate;
+  ratedBy.innerHTML = product.rating.count;
+  price.innerHTML = product.price + " LE";
+  productDescription.innerHTML = product.description;
+  category.innerHTML = product.category;
 }
+async function createProduct() {
+  const clickedProduct = await getProduct();
+  console.log(products);
+
+  initProductImg(clickedProduct);
+  initSmallProductImgs(clickedProduct);
+  initProductInfo(clickedProduct);
+  initProductRating(clickedProduct);
+  initAddToCartBtn(clickedProduct);
+}
+createProduct();
 
 function initPopupAlert(content) {
-    const alertBox = document.createElement('div')
-    const alertContent = document.createTextNode(content)
-    alertBox.setAttribute('id', 'alertBox')
-    alertBox.appendChild(alertContent)
-    document.body.append(alertBox)
+  const alertBox = document.getElementById("added-product");
+  alertBox.className += "border";
+  const alertContent = document.createTextNode(content);
+  alertBox.setAttribute("id", "alertBox");
+  alertBox.appendChild(alertContent);
 }
-
-async function createProduct() {
-    const clickedProduct = await getProduct()
-    
-    initProductImg(clickedProduct)
-    initProductInfo(clickedProduct)
-    initAddToCartBtn(clickedProduct)
+function initAddToCartBtn(product) {
+  const cartBtn = document.getElementById("cart-btn");
+  cartBtn.addEventListener("click", addToLocalStorage);
+  function addToLocalStorage() {
+    const cratIds = localStorage.getItem("cartIds");
+    if (!cratIds) {
+      localStorage.setItem("cartIds", JSON.stringify(product.id));
+    } else {
+      localStorage.setItem("cartIds", `${cratIds},${product.id}`);
+    }
+    initPopupAlert(`Item ${product.title} added successfully to cart`);
+    setTimeout(() => {
+      document.getElementById("alertBox").remove();
+    }, 2000);
+  }
 }
-
-createProduct()
-
-//Test Link Between Two Pages
 function openCartPage() {
-    window.open('../cart/cart.html', '_blank')
+  window.open("../cart/cart.html", "_blank");
 }
-
-document.getElementById('btnn').onclick = openCartPage
